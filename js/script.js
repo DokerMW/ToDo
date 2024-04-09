c7dfc8efb2e6e6abddfd4c1563360a523e3012b7
DokerMW <abddoker@gmail.com>
2024-04-09 10:37:33 +0500

    add: render func

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ const toDoCompleted = document.querySelector('.todo-completed');
 
 const toDoData = [];
 
+const render = () => {
+	toDoList.innerHTML = '';
+	toDoCompleted.innerHTML = '';
+
+	toDoData.forEach(e => {
+		const li = document.createElement('li');
+
+		li.classList.add('todo-item');
+
+		li.innerHTML = 
+		`<span class="text-todo">${e.text}</span>
+		 <div class="todo-buttons">
+			 <button class="todo-remove"></button>
+			 <button class="todo-complete"></button>
+		 </div>`;
+
+		 if (e.completed){
+			toDoCompleted.append(li);
+		 } else {
+			toDoList.append(li);
+		 }
+
+		 li.querySelector('.todo-complete').addEventListener('click', () => {
+			e.completed = !e.completed;
+		 });
+	});
+}
