8f0c6da07b3c2166574e724eb77c68b7ea76a3d2
DokerMW <abddoker@gmail.com>
2024-04-09 10:30:38 +0500

    add: constants

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toDoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const toDoList = document.querySelector('.todo-list');
+const toDoCompleted = document.querySelector('.todo-completed');
+
+const toDoData = [];
+
